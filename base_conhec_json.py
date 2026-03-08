58aa87813305e30d1e7d6d9499e43ce21c317fee
Cabana <filfazjeniffer@gmail.com>
2026-03-07 23:06:54 -0300

    Add files via upload

---
+++ base_conhec_json.py
@@ -0,0 +1,20 @@
+[
+  {
+    "titulo": "checkin",
+    "categoria": "horarios",
+    "tags": ["check-in", "entrada", "horario de entrada"],
+    "texto": "O check-in na cabana é realizado a partir das 17:00."
+  },
+  {
+    "titulo": "checkout",
+    "categoria": "horarios",
+    "tags": ["check-out", "saida"],
+    "texto": "O check-out deve ser realizado até às 15:00."
+  },
+  {
+    "titulo": "lago",
+    "categoria": "natureza",
+    "tags": ["lago", "trilha"],
+    "texto": "A trilha leva até um lago dentro da propriedade."
+  }
+]
